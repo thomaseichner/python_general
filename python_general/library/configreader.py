417473797da94a9f788e77c9c099d5345d78aec7
thomas <Eichner.Thomas1@gmail.com>
2018-03-10 21:12:08 +0100

    Finished configreader

---
+++ python_general/library/configreader.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+"""configreader.py: docstring"""
+
+__author__ = 'thomas'
+__creation_date__ = '09.03.18'
+
+
+import yaml
+
+import python_general.library.baseobject
+
+
+class ConfigReader(python_general.library.baseobject.BaseObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if kwargs.get("config_file", None):
+            config = open(kwargs.get("config_file"), "r")
+            self.config = yaml.load(config)
+            self.log.info(self.config)
